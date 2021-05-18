6a913c92b187412c1353164cda64c80e21befcb2
J27mendes <mendes_j27@hotmail.com>
2021-05-18 19:12:02 -0300

    Adiciona cartazes e traillers dede filmes

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Aluraflix2 - Funções
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/j27mendes/pen/eYgZqeY](https://codepen.io/j27mendes/pen/eYgZqeY).
+
+
---
+++ src/script.js
@@ -0,0 +1,41 @@
+
+function adicionarVideo(){
+  var myVideo=document.getElementById("video") 
+  var videoEmbed = myVideo.value
+  if (videoEmbed.endsWith('embed' , 29)){
+    listarFilmesNaTela(videoEmbed)
+      }else{
+        alert("Video inválido")
+      }
+  myVideo.value = ""
+  function listarFilmesNaTela(video){
+    var listaVideos = document.querySelector('#listaVideos')
+    var elementoVideos = "<iframe src=" + video + ">"
+    listaVideos.innerHTML = listaVideos.innerHTML + elementoVideos
+  }
+  }
+
+
+
+function adicionarFilme() {
+  var campoFilmeFavorito  = document.querySelector('#filme')
+  var filmeFavorito = campoFilmeFavorito.value
+  
+  if (filmeFavorito.endsWith('.jpg')) {         
+   listarFilmesNaTela(filmeFavorito)
+        }else{
+  alert("Imagem inválida")
+ }
+   campoFilmeFavorito.value = ""
+
+function listarFilmesNaTela(filme) {
+  var listaFilmes = document.querySelector('#listaFilmes')
+  var elementoFilme = "<img src=" + filme + ">"
+  listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilme
+}
+}
+
+
+/*Substituir o pôster de cada filme pelo trailer. Dica: pesquisem pela opção embed no menu de compartilhamento do YouTube;
+Estudar o innerHTML e a função que está criando a lista de pôsters. Como o código funciona para adicionar filmes sem utilizar array?
+Criar a função validaFilme() que faça somente a verificação que está sendo feita no if.*/
